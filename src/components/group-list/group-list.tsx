712d50c8c01921defa946796b9689d9edf4b1ea8
Vadim Efimenko <vadik.efimencko@gmail.com>
2020-10-11 21:05:29 +0300

    Переписал компоненты логина, регистрации и ленты (#4)
    Co-authored-by: Vadim Efimenko <vefimenko@ivi.ru>

--- src/components/group-list/group-list.tsx
+++ src/components/group-list/group-list.tsx
@@ -1,34 +1,61 @@
-import React from "react";
-import Form from 'react-bootstrap/Form'
+import React, {useState} from "react";
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
+import * as Icon from 'react-bootstrap-icons';
+
+const RenderCard = () => {
+    const [like, setLike] = React.useState(false); // todo в зависимости от результата сервера
+    const [likeCount, setLikeCount] = React.useState(0); // todo в зависимости от результата сервера
+    const [commentCount, setCommentCount] = React.useState(0); // todo в зависимости от результата сервера
+
+    const onClick = () => {
+        let countLike = likeCount;
+
+        if (like) {
+            setLike(false);
+            setLikeCount(countLike - 1);
+        } else {
+            setLike(true);
+            setLikeCount(countLike + 1);
+        }
+    }
+
+    return (
+        <Card
+            style={{ width: '18rem' }}
+            bg='dark'>
+            <div className='img-wrapper'>
+                <Card.Img variant="top" src="https://i.pinimg.com/originals/26/a2/0a/26a20a99d83cf280fe907a14674c1ad6.png" />
+
+            </div>
+            <Card.Body className='title-pub'>
+                <span className='like-wrapper'>
+                    {
+                        !like ? <Icon.Heart onClick={onClick} size={24} className='like'/> :
+                            <Icon.HeartFill onClick={onClick} size={24} className='like'/>
+                    }
+                    <p className='count-like'>{likeCount}</p>
+                    <Icon.Chat className='pencil-icon' size={24} data-tooltip='Comment' />
+                    <p className='count-like'>{commentCount}</p>
+                </span>
+                <Card.Text className='title'>
+                    Мудрая подпись от инстасамки
+                </Card.Text>
+            </Card.Body>
+            <Card.Text className='pub-data'>
+                <small className="text-muted">10 октября 2020 г.</small>
+            </Card.Text>
+
+        </Card>
+    );
+};
 
 const List = () => {
-    const renderCard = () => {
-        return (
-            <Card
-                style={{ width: '18rem' }}
-                bg='dark'>
-                <Card.Img variant="top" src="https://i.pinimg.com/originals/26/a2/0a/26a20a99d83cf280fe907a14674c1ad6.png" />
-                <Card.Body>c
-                    {/*<Card.Title>Card Title</Card.Title>*/}
-                    <Card.Text>
-                        some text
-                    </Card.Text>
-                    {/*<Button variant="primary">Go somewhere</Button>*/}
-                    <Card.Text>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        );
-    };
-
-    const a = [1,1,1,1,1,1,1,1,1];
+    const a = [1, 1, 1];
 
     return (
         <div>
-            {a.map(item => (renderCard()))}
+            {a.map(item => (RenderCard()))}
         </div>
     );
 };
---
+++ src/components/login-form/RegistrationForm.tsx
@@ -0,0 +1,87 @@
+import React, {useEffect} from "react";
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import {Link} from "react-router-dom";
+
+const RegistrationForm = () => {
+    const [disableButton, setDisableButton] = React.useState(true);
+    const [disableRepInput, setDisableRepInput] = React.useState(true);
+
+    const [firstInputValue, setFirstInputValue] = React.useState('');
+    const [secondInputValue, setSecondInputValue] = React.useState('');
+    const [login, setLogin] = React.useState('');
+
+    useEffect(() => {
+        if (firstInputValue.length || secondInputValue.length) {
+            if (firstInputValue === secondInputValue && login) {
+                setDisableButton(false);
+            } else {
+                setDisableButton(true);
+            }
+        }
+
+    }, [firstInputValue, secondInputValue, login])
+
+    const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const inputValue = event.target.value;
+
+        if (inputValue.length > 0) {
+            setDisableRepInput(false);
+        } else {
+            setDisableRepInput(true);
+        }
+
+        setFirstInputValue(inputValue);
+    }
+
+    const onChangeSeconfInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const inputValue = event.target.value;
+
+        setSecondInputValue(inputValue);
+    }
+
+    const loginChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const inputValue = event.target.value;
+
+        setLogin(inputValue);
+    }
+
+    return (
+        <div className='form'>
+            <div className='login-title'>
+                <h2><span className='first-symbol'>R</span>egistration</h2>
+            </div>
+            <div>
+                <Form className='reg-form'>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Control onChange={loginChange} type="login" placeholder="Login" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Control onChange={onChangeInput} type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Control onChange={onChangeSeconfInput} disabled={disableRepInput} type="password" placeholder="repeat password" />
+                        {
+                            !disableRepInput && (
+                            (firstInputValue.length === secondInputValue.length &&
+                            (firstInputValue !== secondInputValue)) ||
+                            (firstInputValue.length < secondInputValue.length &&
+                                firstInputValue.length > 0 && secondInputValue.length > 0
+                            )) &&
+                            <span className='inline-error'>Incorrect repeated password</span>}
+                    </Form.Group>
+                    <Button disabled={disableButton} variant="primary" type="submit">
+                        Register
+                    </Button>
+                </Form>
+                <div className='switch-link'>
+                    <Link to='/login' >or login</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export { RegistrationForm };
